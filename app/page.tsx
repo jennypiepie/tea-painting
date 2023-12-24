9724c750a461ceb5f3846b464006d2402439f533
派派 <2416655925@qq.com>
2023-12-24 15:17:04 +0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Chat from "@/components/chat/page";
 
 export default async function Home() {
-  const user = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/test`);
-  const { data } = await user.json();
+  // const user = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/test`);
+  // const { data } = await user.json();
 
   return (<>
     {/* <div>TEA{data[0].username}</div> */}
